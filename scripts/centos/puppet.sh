bcd6046e3b2ee3d379d38e811828ebf95af04994
bjanssens <bartjanssens92@gmail.com>
2016-12-30 14:21:07 +0100

    Get the puppet version right
    Signed-off-by: bjanssens <bartjanssens92@gmail.com>

--- scripts/centos/puppet.sh
+++ scripts/centos/puppet.sh
@@ -1,7 +1,14 @@
 #!/usr/vin/env bash
 
-#rpm -ivh https://yum.puppetlabs.com/el/6.5/products/x86_64/puppetlabs-release-6-10.noarch.rpm
-rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-12.noarch.rpm
+if cat /etc/redhat-release | grep 6 &>/dev/null
+then
+  # CentOS 6.8 (Final)
+  echo "Centos 6"
+  rpm -ivh https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-10.noarch.rpm
+else
+  rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-12.noarch.rpm
+fi
+
 sudo yum clean all
 
 # Client
