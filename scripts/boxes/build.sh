22e0ea8236646d3bfb517e9d0b9cfc5245ac4148
bjanssens <bartjanssens92@gmail.com>
2017-01-30 22:17:23 +0100

    Build.sh: Added logging
    Signed-off-by: bjanssens <bartjanssens92@gmail.com>

--- scripts/boxes/build.sh
+++ scripts/boxes/build.sh
@@ -18,15 +18,19 @@ check_params () {
   echo "checking params"
   if [[ -z $base_dir ]]; then base_dir='./'; fi
   if [[ -z $script_dir ]]; then script_dir='./scripts/boxes/'; fi
-  if [[ -z $debug ]]; then debug=false; fi
+  if [[ -z $log_dir ]]; then log_dir='./log'; fi
+  if [[ -z $set_debug ]]; then set_debug=false; fi
   if [[ -z $output_dir ]]; then quit 'Error: Must specify output directory' '3'; fi
   if [[ "${output_dir: -1}" != '/' ]]; then output_dir=$( echo "$output_dir/" ); fi
   if [[ "${script_dir: -1}" != '/' ]]; then script_dir=$( echo "$script_dir/" ); fi
+  if [[ "${log_dir: -1}" != '/' ]]; then log_dir=$( echo "$log_dir/" ); fi
   json_dir="${base_dir}json/"
   build_dir="${base_dir}build/"
   # Check if the output_dir exists,
   # otherwise make it.
-  if [[ ! -d $output_dir ]]; then mkdir -p $output_dir; fi
+  if [[ ! -d $output_dir ]]; then echo "Creating output directory: $output_dir"; mkdir -p $output_dir; fi
+  # Check the log_dir
+  if [[ ! -d $log_dir ]]; then echo "Creating log directory: $log_dir"; mkdir -p $log_dir; fi
 }
 
 # Create tree for boxes
@@ -79,6 +83,18 @@ getcliargs () {
   done
 }
 
+# Debug function
+debug () {
+  if $set_debug; then
+    echo $@
+  fi
+}
+
+# Quit function
+quit () {
+  echo "$1" && exit $2
+}
+
 # Main loop
 # Get the cli options
 getcliargs $@
@@ -91,42 +107,52 @@ available_boxes=$( ls -1 ${json_dir} )
 version=$( date +%y-%m-%d )
 for box in $available_boxes
 do
-	# If headless is set to false, don't build the box
-	headless=$( cat ${json_dir}${box} | grep headless | cut -d ':' -f 2 | sed 's/,$//g' )
+  # If headless is set to false, don't build the box
+  headless=$( cat ${json_dir}${box} | grep headless | cut -d ':' -f 2 | sed 's/,$//g' )
   # Get the name of the box
   boxname=$( echo "$box" | sed 's/.json$//g' )
-	if $headless
-	then
-		# Get the free space in the home mount point via df
-		# Returns the free space in Bytes
-		free_space=$( df ${output_dir} | tail -n 1 | awk '{ print $4 }' )
-		# Make sure that there is at least 1 GB free space before building
-		# 1 GB = 1024^2 = 1048576
-		if (( $free_space < 1048576 ))
-		then
-			echo "ENOSPACE, aborting..."
-			exit 1
-		else
-		        echo "Building box: $box"
-		        packer build ${json_dir}${box}
-            # If the build was successfull
-            # move the box to the correct dir
-            create_tree $boxname
-            # Move the box to the tree
-            echo "Moving box to the tree:"
-            echo "mv ${build_dir}${boxname}.box ${output_dir}/${boxname}/boxes/${boxname}-${version}.box"
-            mv ${build_dir}${boxname}.box ${output_dir}/${boxname}/boxes/${boxname}-${version}.box
-            # Generate the metadata
-            echo "Createing metadata for ${boxname} located in ${output_dir}"
-            if $debug
-            then
-              echo "${script_dir}build-json.py -b ${boxname} -d -o ${output_dir}"
-              ${script_dir}build-json.py -b ${boxname} -d -o ${output_dir}
-            else
-              ${script_dir}build-json.py -b ${boxname} -o ${output_dir}
-            fi
-		        echo "Cleaning cache folder"
-		        rm packer_cache/*.iso
-		fi
-	fi
+  if $headless
+  then
+    # Get the free space in the home mount point via df
+    # Returns the free space in Bytes
+    free_space=$( df ${output_dir} | tail -n 1 | awk '{ print $4 }' )
+    # Make sure that there is at least 1 GB free space before building
+    # 1 GB = 1024^2 = 1048576
+    if (( $free_space < 1048576 ))
+    then
+      echo "ENOSPACE, aborting..."
+      exit 1
+    else
+      # Start the box building
+      echo "Building box: $box"
+      if $set_debug
+      then
+        #echo "packer build ${json_dir}${box}" | tee -a ${log_dir}${boxname}
+        packer build ${json_dir}${box} | tee -a ${log_dir}${boxname}
+      else
+        #echo "packer build ${json_dir}${box}" &> ${log_dir}${boxname}
+        packer build ${json_dir}${box} &> ${log_dir}${boxname}
+      fi
+      # Debuging
+      # touch "${build_dir}${boxname}.box"
+      # If the build was successfull
+      # move the box to the correct dir
+      create_tree $boxname
+      # Move the box to the tree
+      echo "Moving box to the tree:"
+      debug "mv ${build_dir}${boxname}.box ${output_dir}/${boxname}/boxes/${boxname}-${version}.box"
+      mv ${build_dir}${boxname}.box ${output_dir}/${boxname}/boxes/${boxname}-${version}.box
+      # Generate the metadata
+      echo "Createing metadata for ${boxname} located in ${output_dir}"
+      if $set_debug
+      then
+        debug "${script_dir}build-json.py -b ${boxname} -d -o ${output_dir}"
+        ${script_dir}build-json.py -b ${boxname} -d -o ${output_dir} | tee -a ${log_dir}${boxname}
+      else
+        ${script_dir}build-json.py -b ${boxname} -o ${output_dir} &>> ${log_dir}${boxname} 
+      fi
+      debug "Cleaning cache folder"
+      rm packer_cache/*.iso
+    fi
+  fi
 done
