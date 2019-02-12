a43ade7edf4a2313a12e8b4a906ec02b94383f9f
bjanssens <bjanssens@inuits.eu>
2019-02-12 09:26:51 +0100

    Centos7 scripts: Do not include epel by default
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- scripts/centos/centos7-packages.sh
+++ scripts/centos/centos7-packages.sh
@@ -3,7 +3,7 @@
 # Reference:
 # https://wiki.centos.org/HowTos/Virtualization/VirtualBox/CentOSguest
 
-yum install epel-release -y
+#yum install epel-release -y
 yum clean all
 #yum groupinstall "Development Tools" -y
 #yum install kernel-devel -y
